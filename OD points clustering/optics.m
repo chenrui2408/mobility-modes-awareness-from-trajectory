0a4afdf827f73c98c2b7f9fc128cd5c186b15b89
chenrui2408 <37532010+chenrui2408@users.noreply.github.com>
2019-03-13 09:17:25 +0800

    Update optics.m

--- OD points clustering/optics.m
+++ OD points clustering/optics.m
@@ -32,17 +32,6 @@ RD(1)=max(RD(2:m))+.1*max(RD(2:m));
 
 function [D]=dist(i,x)
 
-% function: [D]=dist(i,x)
-%
-% Aim: 
-% Calculates the Euclidean distances between the i-th object and all objects in x	 
-% Input: 
-% i - an object (1,n)
-% x - data matrix (m,n); m-objects, n-variables	    
-%                                                                 
-% Output: 
-% D - Euclidean distance (m,1)
-
 [m,n]=size(x);
 D=(sum((((ones(m,1)*i)-x).^2)'));
 
