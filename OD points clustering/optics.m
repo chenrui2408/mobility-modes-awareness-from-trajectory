e71aa2449e8e7818f8f8ca2aba7aa9969cafdfe8
chenrui2408 <37532010+chenrui2408@users.noreply.github.com>
2019-03-13 09:16:59 +0800

    Update optics.m

--- OD points clustering/optics.m
+++ OD points clustering/optics.m
@@ -1,37 +1,4 @@
-% -------------------------------------------------------------------------
-% Function: 
-% [RD,CD,order]=optics(x,k)
-% -------------------------------------------------------------------------
-% Aim: 
-% Ordering objects of a data set to obtain the clustering structure 
-% -------------------------------------------------------------------------
-% Input: 
-% x - data set (m,n); m-objects, n-variables
-% k - number of objects in a neighborhood of the selected object
-% (minimal number of objects considered as a cluster)
-% -------------------------------------------------------------------------
-% Output: 
-% RD - vector with reachability distances (m,1)
-% CD - vector with core distances (m,1)
-% order - vector specifying the order of objects (1,m)
-% -------------------------------------------------------------------------
-% Example of use:
-% x=[randn(30,2)*.4;randn(40,2)*.5+ones(40,1)*[4 4]];
-% [RD,CD,order]=optics(x,4)
-% -------------------------------------------------------------------------
-% References: 
-% [1] M. Ankrest, M. Breunig, H. Kriegel, J. Sander, 
-% OPTICS: Ordering Points To Identify the Clustering Structure, 
-% available from www.dbs.informatik.uni-muenchen.de/cgi-bin/papers?query=--CO
-% [2] M. Daszykowski, B. Walczak, D.L. Massart, Looking for natural  
-% patterns in analytical data. Part 2. Tracing local density 
-% with OPTICS, J. Chem. Inf. Comput. Sci. 42 (2002) 500-507
-% -------------------------------------------------------------------------
-% Written by Michal Daszykowski
-% Department of Chemometrics, Institute of Chemistry, 
-% The University of Silesia
-% December 2004
-% http://www.chemometria.us.edu.pl
+
 
 function [RD,CD,order]=optics(x,k)
 
